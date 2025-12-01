b2c244101d56057280b70b05048fb1ee8e9da95e
unknown <vaishnavidhavan026@gmail.com>
2025-12-01 15:58:03 +0530

    some routing lessons

--- src/app.js
+++ src/app.js
@@ -2,18 +2,36 @@ const express = require('express')
 
 const app =  express()
 
-app.get("/",(req,res)=> {
-    res.send("welcome to Devtinder")
-})
 
-app.get("/test",(req,res)=> {
+
+app.use("/test",(req,res)=> {
     res.send("testing on application.....")
 })
 
-app.get("/work",(req,res)=> {
-    res.send("workkig.....")
-})
+
+// If the sequence is chnage then the routing is also chnage
+// app.use("/",(req,res)=> {
+//     res.send("welcome to Devtinder")
+// })
+
+// app.get("/abc",(req,res)=> {
+//     res.send("this is abc route")
+// })
+
+// app.get("/ab*c",(req,res)=> {
+//     res.send("b multiplices")   // abbc, abbc, abbbbc
+// })
+
+
 
 app.listen(7777,() => {
     console.log("server listening on port 7777");   
 })
+
+// query parameters = "/api/users?name=John&age=30"
+// console.log(req.query)
+
+
+// for route parameters = "/api/users/:id"
+// console.log(req.params)
+
